b22ed148d13a9d2ee2cbed6d93b228f069f387d5
Fukuemon <Fukuemony029@gmail.com>
2023-09-14 22:22:29 +0900

    🔥 フォロー・フォロー解除のurl削除

--- apps/profiles/urls.py
+++ apps/profiles/urls.py
@@ -9,8 +9,6 @@ router.register("profile", views.ProfileViewSet)
 
 urlpatterns=[
     path("myprofile/", views.MyProfileListView.as_view(), name="myprofile"),
-    path('<slug:username>/follow', views.FollowView.as_view(), name='follow'),
-    path('<slug:username>/unfollow', views.UnfollowView.as_view(), name='unfollow'),
     path("",include(router.urls)),
 ]
 
