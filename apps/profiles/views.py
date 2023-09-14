1cc5d4d52682b771ef74cae90ea5ac13d489f55d
Fukuemon <Fukuemony029@gmail.com>
2023-09-14 13:25:37 +0900

    ✨ProfileのView作成 #1
    ProfileViewSet
    viewsetsを継承し、crud操作を可能とする
    perform_create
    新規でオブジェクトを作成する際に処理を加える
    この場合は、現在ログインしているユーザーをパラメータに当てはめる

--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -1,3 +1,30 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+from .models import Profile, Connection
+from .serializers import  ConnectionSerializer
+from rest_framework import viewsets
+from . import serializers
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userProfile=self.request.user)
+
+
+class MyProfileListView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+    def get_queryset(self):
+        return self.queryset.filter(userProfile=self.request.user)
+
+
+class FollowView(generics.CreateAPIView):
+    queryset = Connection.objects.all()
+    serializer_class = ConnectionSerializer
+
+class UnfollowView(generics.DestroyAPIView):
+    queryset = Connection.objects.all()
+    serializer_class = ConnectionSerializer
