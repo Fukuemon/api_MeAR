68981462212a13fecf5c64f2a0c7ec4b881fe341
Fukuemon <Fukuemony029@gmail.com>
2023-09-15 00:48:44 +0900

    ⚡️ serializers.pyにて、コネクト先のプロフィール情報も表示 #1
    ProfileBriefSerializerで、取り扱いたいプロフィールの情報を指定する
    
    ConnectionSerializerで、followerとfollowingに上記のシリアライザーを継承させる
    
    ProfileSerializerで、Profileオブジェクトがフォローしているユーザーとフォローされているユーザーのリストを取得

--- apps/profiles/serializers.py
+++ apps/profiles/serializers.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
 from .models import Profile, Connection
 
+# プロフィールの一部のフィールドを扱うためのシリアライザー
+class ProfileBriefSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['nickName', 'created_on', 'img']
+
 # フォロー機能のシリアライザー
 class ConnectionSerializer(serializers.ModelSerializer):
+    follower = ProfileBriefSerializer(source='follower', read_only=True)
+    following = ProfileBriefSerializer(source='following', read_only=True)
+
     class Meta:
         model = Connection
         fields = ['follower', 'following']
+
 
 
 # プロフィールモデルにフォローとフォロワーを格納する
@@ -22,11 +32,14 @@ class ProfileSerializer(serializers.ModelSerializer):
     def get_followings(self, obj):
         # プロフィールがフォローしている人をシリアライズ
         followings = Connection.objects.filter(follower=obj)
-        return ConnectionSerializer(followings, many=True).data
+        following_profiles = [connection.following for connection in followings]
+        return ProfileBriefSerializer(following_profiles, many=True).data
 
     def get_followers(self, obj):
         # プロフィールをフォローしている人をシリアライズ
         followers = Connection.objects.filter(following=obj)
-        return ConnectionSerializer(followers, many=True).data
+        follower_profiles = [connection.follower for connection in followers]
+        return ProfileBriefSerializer(follower_profiles, many=True).data
+
 
 
