7ba3e33b2b40c4b9ac5ff2c1fb1a6bec56372ec1
Fukuemon <Fukuemony029@gmail.com>
2023-09-28 15:08:56 +0900

    ✨ユーザー削除とパスワード変更のエンドポイント作成 #1
    djoserからユーザー削除とパスワード変更のエンドポイントをインポート
    ユーザー削除のエンドポイントでは、少しカスタマイズして、ログインユーザーのアカウントを削除するように

--- apps/authn/urls.py
+++ apps/authn/urls.py
@@ -1,13 +1,19 @@
 from django.urls import path, include
 from apps.authn import views
 from rest_framework.routers import DefaultRouter
+from djoser.views import UserViewSet
+
 
 app_name = "user"
 
 router = DefaultRouter()
 
+
 # generics用
 urlpatterns = [
     path("register/", views.CreateUserView.as_view(), name="register"),
-    path("",include(router.urls))
+    # ログインユーザーのアカウント削除のエンドポイント
+    path('users/delete/', views.CustomUserViewSet.as_view(actions={'delete': 'delete_current_user'}), name='user-delete'),
+    # パスワード変更のエンドポイント
+    path('change_password/', UserViewSet.as_view(actions={'post': 'set_password'}), name='set-password'),
 ]
--- apps/authn/views.py
+++ apps/authn/views.py
@@ -3,6 +3,11 @@ from rest_framework import generics
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from . import serializers
+from djoser.views import UserViewSet as DjoserUserViewSet
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import action
+
 from .models import User
 # Create your views here.
 
@@ -11,3 +16,11 @@ class CreateUserView(generics.CreateAPIView):
     permission_classes = (AllowAny,)
 
 
+
+class CustomUserViewSet(DjoserUserViewSet):
+
+    # ログイン中のユーザー削除のエンドポイント
+    @action(detail=False, methods=['DELETE'])
+    def delete_current_user(self, request):
+        request.user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
